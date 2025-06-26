e1d93ddd433c1e7e595e4c9bbb972cca9aa1e431
Rohit030919 <garjerohit127@gmail.com>
2025-06-26 18:07:03 +0530

    Initial server commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
+    socket.to(roomId).emit('opponent-joined');
+  });
+
+  socket.on('progress', ({ roomId, index }) => {
+    socket.to(roomId).emit('opponent-progress', index);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
